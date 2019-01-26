5c417246905a0e69e99901f016224a447a519fd0
Ricardo <Ricardo@RICARDO>
2019-01-26 17:44:49 +0100

    Merge origin/master
    Conflicts:
    	nbactions.xml

---
+++ src/main/java/dao/DBConnectionPool.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import config.ConfiguracionYaml;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+
+
+public class DBConnectionPool {
+
+    private static DBConnectionPool dbconection = null;
+
+    private DataSource hirakiDatasource = null;
+
+    DBConnectionPool() {
+        hirakiDatasource = getDataSourceHikari();
+    }
+
+    public static DBConnectionPool getInstance() {
+        if (dbconection == null) {
+            dbconection = new DBConnectionPool();
+        }
+
+        return dbconection;
+    }
+
+    public Connection getConnection() throws Exception {
+        Class.forName(ConfiguracionYaml.getInstance().getDriverDB());
+        Connection connection;
+        connection = hirakiDatasource.getConnection();
+
+        return connection;
+    }
+
+
+    private DataSource getDataSourceHikari() {
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(ConfiguracionYaml.getInstance().getUrlDB());
+        config.setUsername(ConfiguracionYaml.getInstance().getUserDB());
+        config.setPassword(ConfiguracionYaml.getInstance().getPassDB());
+        config.setDriverClassName(ConfiguracionYaml.getInstance().getDriverDB());
+        config.setMaximumPoolSize(10);
+
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource datasource = new HikariDataSource(config);
+
+        return datasource;
+    }
+
+    public DataSource getDataSource() {
+       
+        return hirakiDatasource;
+    }
+
+    public void cerrarConexion(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.setAutoCommit(true);
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnectionPool.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void cerrarPool() {
+       hirakiDatasource = null;
+    }
+}
--- src/main/java/dao/implementaciones/DAOTipoUsuarioImpl.java
+++ src/main/java/dao/implementaciones/DAOTipoUsuarioImpl.java
@@ -6,9 +6,15 @@
 package dao.implementaciones;
 
 import dao.DAOTipoUsuario;
+import dao.DBConnectionPool;
+import java.sql.Connection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.TipoUsuario;
-
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 public class DAOTipoUsuarioImpl implements DAOTipoUsuario {
 
@@ -34,12 +40,31 @@ public class DAOTipoUsuarioImpl implements DAOTipoUsuario {
 
     @Override
     public List<TipoUsuario> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<TipoUsuario> lista = null;
+        Connection con = null;
+
+        try {
+            con = DBConnectionPool.getInstance().getConnection();
+
+            QueryRunner qr = new QueryRunner();
+
+            ResultSetHandler<List<TipoUsuario>> handler = new BeanListHandler<>(TipoUsuario.class);
+
+            lista = qr.query(con, "SELECT * FROM tipousuario", handler);
+
+        } catch (Exception ex) {
+            Logger.getLogger(DAOTipoUsuarioImpl.class.getName()).log(Level.SEVERE, null, ex);
+
+        } finally {
+
+            DBConnectionPool.getInstance().cerrarConexion(con);
+        }
+        return lista;
     }
 
     @Override
     public TipoUsuario obtener(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
